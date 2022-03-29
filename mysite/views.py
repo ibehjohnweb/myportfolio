ca75530434a7f83483b378fff5897492ddba82b1
Ibeh John <ibehjohnweb@gmail.com>
2022-03-29 17:26:30 +0800

    First version of my portfolio moved to github

---
+++ mysite/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render, redirect
+from django.core.mail import send_mail
+
+
+def home(request):
+    if request.method == "POST":
+        name = request.POST['fullname']
+        email = request.POST['email']
+        message = request.POST['message']
+        contacting = "Name: " + name + " \nMessage: " + message + " \nEmail:" + email
+        # send an email
+        send_mail(
+            'Contact on portfolio',  # subject
+            contacting,  # message
+            email,  # from email
+            ['ibehjohn16@gmail.com'],  # to email
+            fail_silently=False,
+        )
+        return render(request, 'home.html', {'name': name})
+    else:
+        return render(request, 'home.html', {})
+
